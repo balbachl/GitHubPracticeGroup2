183f0d4de8f9e58b5ed99f39bfe19a040e82e934
hvittorelli <misshopev@gmail.com>
2025-10-16 16:43:56 -0400

    Added Email to Edit Pages

---
+++ Migrations/20251016204106_AddEmailtoEditPage.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GitHubPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailtoEditPage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Memberships",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Email",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Memberships");
+        }
+    }
+}
